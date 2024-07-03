92d86ed0cd6448daf6fb9496a8ef91e9ae12093b
abhi114 <nabhi531@gmail.com>
2024-07-03 20:54:33 +0530

    added header and options

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -37,7 +37,7 @@ const handleMouseEnter =(row:number, col:number)=>{
 }
 return (
     <div className={twMerge(//base classes
-        "flex items-center flex-col justify-center border-sky-300",
+        "flex items-center flex-col justify-center border-sky-300 mt-10 pb-10",
         //control grid height
         `lg:min-h-[${MAX_ROWS*17}px] md:min-h-[${MAX_ROWS*15}px] 
         xs:min-h-[${MAX_ROWS*8}px] min-h-[${MAX_ROWS*7}px]`,
